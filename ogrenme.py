ff5b0695eddec76f9ff01fd0b61cc80d8cdce4f7
y-eren <kynxaresopollon@gmail.com>
2021-12-07 21:53:16 +0300

    Dosyalar gönderildi

---
+++ ogrenme.py
@@ -0,0 +1,59 @@
+"""  PYTHON OGRENME PROGRAMI  """
+
+""" yazilim = "yazilim"
+
+print(yazilim * 3)
+
+print (yazilim, 3)
+
+a = "Yazilim Bilimi"
+
+print(a[:10])
+ """
+
+
+
+""" LİSTELER """
+""" oyuncu1 = ["Arda", "Turan", 28, 1, 77]
+
+print(oyuncu1)
+print(oyuncu1[1])
+
+oyuncu1[1] = "Uran"
+
+print(oyuncu1[1])
+
+oyuncu1.append("Barcelona")
+oyuncu1[:2]= ["Dani", "Alves"]
+print(oyuncu1[:2]) """
+
+""" print("Oyuncunun adı", ad, "Oyuncunun Soyadı", soyad, "Oyuncunun Oynadığı Takım", takim) """
+""" bilgiler = [ad, soyad, takim]
+print("Oyuncunun adı : {}\n Oyuncunun Soyadı {}".format(bilgiler[0])) """
+
+""" string = "gollum"
+liste = ["elma", "armut", "kiraz"]
+
+for i in liste:
+    print(i) """
+
+""" print(*range(2, 10, 3)) """
+
+""" PYTHON DA FONKSİYON TANIMLAMA  """
+
+""" def şeklinde tanımlanır. """
+
+""" 
+def kayit_ekle(ad="bilgi yok", soyad="bilgi yok", yas="bilgi yok", meslek="bilgi yok"):
+    Bu şekilde varsayılan değer olarak yazılabillir fonksiyonu çağırırkende değişken yazılmadan sırasıyla
+    çağrılabilir veyahut değişken ismi yazılarak ama sırasız bir şekilde de yapılabilir. """
+
+""" global keyword javascript var gibi çalışıyor """
+
+
+
+
+
+
+
+ 
